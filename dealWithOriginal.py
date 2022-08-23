8394dc3eaad98aa63e435c136ceda5108a86208f
marsxinwang <marsxinwang@tencent.com>
2022-08-23 23:23:22 +0800

    feat:好事的蠢方法

--- dealWithOriginal.py
+++ dealWithOriginal.py
@@ -18,7 +18,6 @@ def read_data():
 
     # 判断是否存在，没有的话立刻创建，用于保存结果
     result_sheet_name = '结果'
-    #
     if result_sheet_name in book.sheetnames:
         # remove表格
         result_sheet = book[result_sheet_name]
@@ -26,7 +25,6 @@ def read_data():
 
     # 创建一张新的sheet, 用于存放处理完毕的数据
     result_sheet = book.create_sheet(result_sheet_name, 1)
-    # book.save('original.xlsx')
 
     # 做一些准备工作
     start_year_index_str = 'B'
@@ -39,36 +37,56 @@ def read_data():
     start_year_str = data_source_sheet[start_year_index_str + '5']
     print('start_year_str = ' + str(start_year_str.value.year))
 
-    # 营业收入
-    ds_business_income_cell_tuple = tuple(['B', 12])
-
+    ds_business_income_cell_tuple = tuple(['B', '12']) # 营业收入
+    ds_business_cash_flow_cell_tuple = tuple(['B', '244']) # '经营活动现金流入'
+    ds_business_net_profit_cell_tuple = tuple(['B', '62']) # '净利润'
+    ds_business_net_cash_cell_tuple = tuple(['B', '275']) # '经营活动产生的现金流量净额'
+    ds_business_net_fetch_money_cell_tuple = tuple(['B', '318']) # '筹资净额（亿元）'
 
     # 配置第一行的titles
-    first_row = ['年份', '营业收入', '经营活动现金流入', '净利润', '经营活动产生的现金流量净额', '筹资净额', '投资净额', '营业成本%', '营业税金及附加%']
-    # for index in range(len(first_row)):
-        # result_sheet['B' + '1'] = first_row[index]
+    first_row = ['年份',
+                 '营业收入（亿元）',
+                 '经营活动现金流入（亿元）',
+                 '净利润（亿元）',
+                 '经营活动产生的现金流量净额（亿元）',
+                 '筹资净额（亿元）',
+                 '投资净额（亿元）',
+                 '营业成本%',
+                 '营业税金及附加%']
     result_sheet.append(first_row)
+
+    yi_unit = 100000000.0
     # 配置每一列数据
     for index in range(data_source_years):
-        current_row = 'A' + str(index + 2)
-        result_sheet[current_row] = str(start_year_str.value.year + index)
+        # 1列：拼写当前年
+        result_sheet['A' + str(index + 2)] = str(start_year_str.value.year + index)
 
-        # 填写营业收入
-        test = ds_business_income_cell_tuple[0]
-        # 数据源test需要加1
-        print('B12=', data_source_sheet['B12'].value)
-        # print('chr(ord(test) + 1) + 12', chr(ord(test) + index) + '12')
-        income = data_source_sheet[chr(ord(test) + index) + '12'].value / 100000000.0
-        format_in_come = str(income).split('.')[0] + '.' + str(income).split('.')[1][:2]
-        result_sheet[test + str(index + 2)] = format_in_come
+        # 2列：填写营业收入
+        income = data_source_sheet[chr(ord(ds_business_income_cell_tuple[0]) + index) + ds_business_income_cell_tuple[1]].value / yi_unit
+        result_sheet['B' + str(index + 2)] = two_formate(income)
 
-        print('chr(ord(test) + 1) + 12 = ', income)
+        # 3列：营业收入
+        income = data_source_sheet[chr(ord(ds_business_cash_flow_cell_tuple[0]) + index) + ds_business_cash_flow_cell_tuple[1]].value / yi_unit
+        result_sheet['C' + str(index + 2)] = two_formate(income)
+
+        # 4列:净利润
+        income = data_source_sheet[chr(ord(ds_business_net_profit_cell_tuple[0]) + index) + ds_business_net_profit_cell_tuple[1]].value / yi_unit
+        result_sheet['D' + str(index + 2)] = two_formate(income)
+
+        # 5列:经营活动产生的现金流量净额（亿元
+        income = data_source_sheet[chr(ord(ds_business_net_cash_cell_tuple[0]) + index) + ds_business_net_cash_cell_tuple[1]].value / yi_unit
+        result_sheet['E' + str(index + 2)] = two_formate(income)
+
+        # 6列:筹资净额（亿元
+        income = data_source_sheet[chr(ord(ds_business_net_fetch_money_cell_tuple[0]) + index) + ds_business_net_fetch_money_cell_tuple[1]].value / yi_unit
+        result_sheet['F' + str(index + 2)] = two_formate(income)
+
 
     # 保存下
     book.save('original.xlsx')
 
-    # 具体科目
-    # 营业收入
+def two_formate(income):
+    return str(income).split('.')[0] + '.' + str(income).split('.')[1][:2]
 
 
 if __name__ == '__main__':
