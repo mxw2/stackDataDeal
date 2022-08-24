a2b3e793ee4e1de07da02f623665463240c0bc2f
marsxinwang <marsxinwang@tencent.com>
2022-08-24 18:22:32 +0800

    删除无用代码

--- column_model.py
+++ column_model.py
@@ -8,29 +8,38 @@ class CalculateType(Enum):
     DivisionIncome = 3  # 除以营业收入
 
 
+useful_years_default = 10
+useful_years_6 = 6
+useful_years_4 = 4
+
+
 class ColumnModel:
-    def __init__(self, name, ds_row_index, text_color, calculate_type):
+    # useful_years 显示几年，默认10年
+    def __init__(self, name, ds_row_index, text_color, calculate_type, useful_years):
         self.name = name
         self.ds_row_index = ds_row_index
         self.text_color = text_color
         self.calculate_type = calculate_type
+        self.useful_years = useful_years
 
 
 def want_to_deal_with_data_source():
-    years = ColumnModel('年份', 5, None, CalculateType.Year)
-    income = ColumnModel('营业收入（亿元）', 12, None, CalculateType.OriginalData)
-    cash_flow = ColumnModel('经营活动现金流入（亿元）', 244, None, CalculateType.OriginalData)
-    net_profit = ColumnModel('净利润', 62, None, CalculateType.OriginalData)
-    business_net_cash = ColumnModel('经营活动产生的现金流量净额', 275, None, CalculateType.OriginalData)
-    net_money_in = ColumnModel('筹资净额（亿元）', 318, None, CalculateType.OriginalData)
-    net_money_out = ColumnModel('投资净额（亿元）', 297, None, CalculateType.OriginalData)
-    business_money_in = ColumnModel('营业成本%', 19, None, CalculateType.DivisionIncome)
+    years = ColumnModel('年份', 5, None, CalculateType.Year, useful_years_default)
+    income = ColumnModel('营业收入（亿元）', 12, None, CalculateType.OriginalData, useful_years_default)
+    cash_flow = ColumnModel('经营活动现金流入（亿元）', 244, None, CalculateType.OriginalData, useful_years_default)
+    net_profit = ColumnModel('净利润', 62, None, CalculateType.OriginalData, useful_years_default)
+    business_net_cash = ColumnModel('经营活动产生的现金流量净额', 275, None, CalculateType.OriginalData, useful_years_default)
+    business_net_cash_short = ColumnModel('经营活动净额(亿元)', 275, "ff0000", CalculateType.OriginalData, useful_years_6)
+    net_money_in = ColumnModel('筹资净额（亿元）', 318, "ff0000", CalculateType.OriginalData, useful_years_6)
+    net_money_out = ColumnModel('投资净额（亿元）', 297, "ff0000", CalculateType.OriginalData, useful_years_6)
+    business_money_in = ColumnModel('营业成本%', 19, None, CalculateType.DivisionIncome, useful_years_4)
 
     return [years,
             income,
             cash_flow,
             net_profit,
             business_net_cash,
+            business_net_cash_short,
             net_money_in,
             net_money_out,
             business_money_in]
--- dealWithOriginal.py
+++ dealWithOriginal.py
@@ -3,7 +3,7 @@ from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Align
 import column_model
 from column_model import CalculateType
 
-# preprare data
+# prepare data
 book = openpyxl.load_workbook('original.xlsx')
 # 数据源sheet
 ds_sheet = book["利润表,资产负债表,现金流量表1"]
@@ -11,6 +11,9 @@ ds_sheet = book["利润表,资产负债表,现金流量表1"]
 start_year_index_char = 'B'
 # 数据源开始年的end char
 end_year_index_char = 'J'
+# income在ds中的index
+ds_income_row = 0
+
 
 def create_result_sheet():
     # 判断是否存在，没有的话立刻创建，用于保存结果
@@ -22,6 +25,7 @@ def create_result_sheet():
 
     # 创建一张新的sheet, 用于存放处理完毕的数据
     return book.create_sheet(result_sheet_name, 1)
+
 
 def read_data():
     # 查看下有数据单元格范围，表格不是特别准确，可能是我之前wb做了很多无用功导致的，以后在试试
@@ -37,11 +41,6 @@ def read_data():
     # result 会有（data_source_years + 1）列
     data_source_years = ord(end_year_index_char) - ord(start_year_index_char) + 1
     print('共有' + str(data_source_years) + '年')
-
-    # 获取开始年份
-    # start_year_str = ds_sheet[start_year_index_char + '5']
-    # print('start_year_str = ' + str(start_year_str.value.year))
-    # result_sheet.append(first_row)
 
     # 获取要处理的数据
     column_models = column_model.want_to_deal_with_data_source()
@@ -64,71 +63,37 @@ def read_data():
             # result每次从ds中获取数据的时候，result每次row + 1，datasource每次column + 1
             ds_cell_index = chr(row + ord(start_year_index_char)) + str(model.ds_row_index)
             result_cell_index = chr(column + ord('A')) + str(row + 2)
+            content = ''
             if model.calculate_type == CalculateType.Year:
                 year_str = ds_sheet[ds_cell_index]
-                result_sheet[result_cell_index] = year_str.value.year
+                content = year_str.value.year
             elif model.calculate_type == CalculateType.OriginalData:
                 original_data = ds_sheet[ds_cell_index].value
-                result_sheet[result_cell_index] = two_formate(original_data / yi_unit)
+                content = two_formate(original_data / yi_unit)
             elif model.calculate_type == CalculateType.DivisionIncome:
                 original_data = ds_sheet[ds_cell_index].value
                 # 获取当年的营业搜索cell索引
                 income_cell_index = chr(row + ord(start_year_index_char)) + '12'
                 income_data = ds_sheet[income_cell_index].value
-                result_sheet[result_cell_index] = two_formate(original_data / income_data) * 100
+                content = two_formate(original_data / income_data) * 100
+            else:
+                content = ''
 
-    # 建议修改，数组都以1开始即可
-    # 配置每一列数据
-    # for index in range(data_source_years):
-    #     # 数据源中开始遍历的列的index值
-    #     ds_begin_colume_char = 'B'
-    #     # 1列：拼写当前年
-    #     result_sheet['A' + str(index + 2)] = str(start_year_str.value.year + index)
-    #
-    #     ds_current_column_char = ord(ds_begin_colume_char) + index
-    #     # 2列：填写营业收入
-    #     income_true = data_source_sheet[chr(ds_current_column_char) + ds_business_income_cell_row].value / yi_unit
-    #     result_sheet['B' + str(index + 2)] = two_formate(income_true)
-    #
-    #     # 3列：营业收入
-    #     income = data_source_sheet[chr(ds_current_column_char) + ds_cash_flow_cell_row].value / yi_unit
-    #     result_sheet['C' + str(index + 2)] = two_formate(income)
-    #
-    #     # 4列:净利润
-    #     income = data_source_sheet[chr(ds_current_column_char) + ds_net_profit_cell_row].value / yi_unit
-    #     result_sheet['D' + str(index + 2)] = two_formate(income)
-    #
-    #     # 5列:经营活动产生的现金流量净额【要10年 & 要6年】
-    #     income = data_source_sheet[chr(ds_current_column_char) + ds_net_cash_cell_row].value / yi_unit
-    #     result_sheet['E' + str(index + 2)] = two_formate(income)
-    #
-    #     if data_source_years - index <= 6:
-    #         cell = result_sheet['E' + str(index + 2)]
-    #         cell.fill = PatternFill("solid", fgColor="ff0000")
-    #         # 6列:筹资净额【只要6年的】
-    #         income = data_source_sheet[chr(ds_current_column_char) + ds_net_money_in_cell_row].value / yi_unit
-    #         result_sheet['F' + str(index + 2)] = two_formate(income)
-    #         cell = result_sheet['F' + str(index + 2)]
-    #         cell.fill = PatternFill("solid", fgColor="ff0000")
-    #
-    #         # 7列:投资净额【只要6年的】
-    #         income = data_source_sheet[chr(ds_current_column_char) + ds_net_money_out_cell_row].value / yi_unit
-    #         result_sheet['G' + str(index + 2)] = two_formate(income)
-    #         cell = result_sheet['G' + str(index + 2)]
-    #         cell.fill = PatternFill("solid", fgColor="ff0000")
-    #
-    #     if data_source_years - index <= 4:
-    #         # 8列:'营业成本%'【只要4年】
-    #         income = data_source_sheet[chr(ds_current_column_char) + ds_operating_cost_cell_row].value / yi_unit
-    #         result_sheet['H' + str(index + 2)] = two_formate(income / income_true * 100)
+            result_sheet[result_cell_index] = content
+            cell = result_sheet[result_cell_index]
+            # 统一设置文字颜色等
+            if model.text_color is not None:
+                cell.fill = PatternFill("solid", fgColor=model.text_color)
 
     # 保存下
     book.save('original.xlsx')
 
+
 # 返回两位小数数字
 def two_formate(income):
-    return str(income).split('.')[0] + '.' + str(income).split('.')[1][:2]
-    # return int(result_str)
+    result_str = str(income).split('.')[0] + '.' + str(income).split('.')[1][:2]
+    return float(result_str)
+
 
 if __name__ == '__main__':
     read_data()
