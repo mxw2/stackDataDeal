affba447f6bb4e2806aa9d640b1fa471158d0d68
wangxin <1743998535@qq.com>
2024-04-20 20:45:43 +0800

    代码重构

--- deal_with_data/daily_retracement/history_max_loss.py
+++ deal_with_data/daily_retracement/history_max_loss.py
@@ -8,15 +8,15 @@ target_days = 5
 # f1fe4-ssf8u 1998-2017
 # 2o2bi-e3f13 2017-2024
 
-from daily_price_info import DailyPriceInfo
+from price_info import PriceInfo
 import openpyxl
 import math
 
-workbook_name = 'f1fe4-ssf8u.xlsx'
+workbook_name = 'AAPL_11_24.xlsx'
 # prepare data
 book = openpyxl.load_workbook(workbook_name)
 # 数据源sheet
-ds_sheet = book["f1fe4-ssf8u"]
+ds_sheet = book["AAPL_11_24"]
 
 # 从第二行开始读数据
 row_index_for_start_read = 2
@@ -27,13 +27,13 @@ column_index_for_min_value = 'E'
 # 时间排序好的价格模型
 price_infos = []
 
-max_loss_percent: float = 0.0
+# 统计全局的最大损失值，和每一天for 4个寻找的max做对比，最大的放到这里即可
+# history_max_loss_price_info: PriceInfo
 
 
 # ********************************** 读取表内容 **********************************
 def read_data():
     # 读取数据源表，并且做若干判断，保证数据位置都是正确的
-
     print("Maximum row: {0}, 有效行数量:{1}".format(ds_sheet.max_row, ds_sheet.max_row - 1))
     print("Maximum column: {0}".format(ds_sheet.max_column))
 
@@ -44,66 +44,69 @@ def read_data():
         date = ds_sheet[column_index_for_date + str(i)].value
         min_value = ds_sheet[column_index_for_min_value + str(i)].value
         max_value = ds_sheet[column_index_for_max_value + str(i)].value
-        price_info = DailyPriceInfo(date, max_value, min_value)
+        price_info = PriceInfo(date, max_value, min_value)
         price_infos.append(price_info)
 
     # 按照时间由远到近的排序
     price_infos.reverse()
 
 
-def max_retracement():
+def max_loss():
     # 统计最大亏损
-    max_loss = 0
-    global max_loss_percent
-    # 这里可以使用2个info持有
-    max_loss_start_price_info = None
-    max_loss_end_price_info = None
+    history_max_loss_price_info: PriceInfo = None
 
     # 遍历每个价格 & 求出连续5天最大回撤
     for i in range(len(price_infos)):
         current_price_info = price_infos[i]
         # 【压力测试】买在当前最高价、触碰后4日最低价
         current_price = current_price_info.max_value
+
         for j in range(i + 1, i + target_days):
             # 特殊处理，保证j < price_infos.count
             if j >= len(price_infos):
                 # print("遍历价格数组越界打印：i :{0}, j :{1}，continue".format(i, j))
                 continue
-            other_price_info = price_infos[j]
+            temp_price_info = price_infos[j]
             # 使用最小值，便于压力测试，必须问问券商保证金随股价变化公式
-            other_price = other_price_info.min_value
-            if other_price < current_price:
-                temp_loss = round(other_price - current_price, 2)
-                temp_loss_percent = round(temp_loss / current_price, 4)
-                # 这里不可以使用损失的钱数比较，要采用百分比比较才可以
-                if temp_loss_percent < max_loss_percent:
-                    max_loss = temp_loss
-                    max_loss_percent = temp_loss_percent
-                    max_loss_start_price_info = current_price_info
-                    max_loss_end_price_info = other_price_info
-                    # 给current_price_info缓存最大回撤百分比
-                    current_price_info.retracement_percent = temp_loss_percent
-                    current_price_info.debug_log(i)
+            temp_price = temp_price_info.min_value
 
+            if temp_price < current_price:
+                temp_loss_money = round(temp_price - current_price, 2)
+                temp_loss_percent = round(temp_loss_money / current_price, 4)
+                # 这里统计的是current_price_info对比4天寻找最大的损失并存储到current_price_info模型中
+                # 最后整体再和历史上最大的损失百分比进行比较即可
+                # 【注意】不可以使用损失的钱数比较，要采用百分比才可以
+                if temp_loss_percent < current_price_info.loss_percent:
+                    current_price_info.loss_percent = temp_loss_percent
+                    current_price_info.loss_money = temp_loss_money
+                    current_price_info.loss_price_info = temp_price_info
+
+        # 此时current_price_info已经和后4天比较完毕最大损失
+        # 和历史上最大损失进行比较
+        if (history_max_loss_price_info is None or
+                current_price_info.loss_percent < history_max_loss_price_info.loss_percent):
+            # 【注意】此时回撤百分比是最小的，但是回撤价格不一定，因为股价不同，不能直接用价格比较
+            history_max_loss_price_info = current_price_info
 
     debug_log()
 
     # 打印最后的结果哈
     print("---------------------")
     describe_str = "压力测试:【最大值-最小值】\n"
-    max_loss_str = f"最大损失: {str(max_loss)}\n"
-    max_loss_percent_str = f"最大损失百分比: {str(round(max_loss_percent * 100, 2))}% \n"
-    max_loss_start_str = f"开始日:{max_loss_start_price_info.date},最高价: {str(max_loss_start_price_info.max_value)}\n"
-    max_loss_end_str = f"损失日: {max_loss_end_price_info.date},最低价: {str(max_loss_end_price_info.min_value)}\n"
+    max_loss_str = f"最大损失: {history_max_loss_price_info.loss_money}\n"
+    max_loss_percent_str = f"最大损失百分比: {history_max_loss_price_info.loss_percent_str()}\n"
+    max_loss_start_str = f"开始日:{history_max_loss_price_info.date}, 最高价: {history_max_loss_price_info.max_value}\n"
+    history_max_loss_end_price_info = history_max_loss_price_info.loss_price_info
+    max_loss_end_str = f"损失日: {history_max_loss_end_price_info.date},最低价: {history_max_loss_end_price_info.min_value}\n"
     print(describe_str + max_loss_str + max_loss_percent_str + max_loss_start_str + max_loss_end_str)
 
 
 def loss_distributions():
-    global max_loss_percent
+    global history_max_loss_percent
     # 如果最大亏损为-3.7%，则需要4个位置
     # 向上取整
     scale = 100
-    count = math.ceil(-max_loss_percent * scale)
+    count = math.ceil(-history_max_loss_percent * scale)
     loss_percent_distributes = [0] * count
     for i in range(len(price_infos)):
         current_price_info = price_infos[i]
@@ -134,5 +137,5 @@ def debug_log():
 # ********************************** 启动 **********************************
 if __name__ == '__main__':
     read_data()
-    max_retracement()
-    loss_distributions()
+    max_loss()
+    # loss_distributions()
--- deal_with_data/daily_retracement/price_info.py
+++ deal_with_data/daily_retracement/price_info.py
@@ -1,10 +1,13 @@
-class DailyPriceInfo:
+from datetime import datetime
+
+
+class PriceInfo:
     def __init__(self, date, max_value, min_value):
         assert max_value > 0, "max_value must be greater than 0"
         assert min_value > 0, "min_value must be greater than 0"
         if isinstance(date, str):
             self.date = date
-        elif isinstance(date, date):
+        elif isinstance(date, datetime):
             self.date = date.strftime("%Y-%m-%d")
         else:
             raise TypeError("我也不知道什么类型 = ", type(date))
@@ -13,8 +16,16 @@ class DailyPriceInfo:
         self.min_value = min_value
         # 保留2位小数
         self.average_price = round((max_value + min_value) * 0.5, 2)
-        # 应该是负数百分比
-        self.retracement_percent = 0.0
+        # 应该是负数百分比，小数（未变成百分比）
+        self.loss_percent = 0.0
+        # 下降了多少钱，是负数
+        self.loss_money = 0.0
+        # 暴跌日价格信息
+        self.loss_price_info: PriceInfo = None
+
+    # 获取损失百分比字符串，会对
+    def loss_percent_str(self):
+        return f"{round(self.loss_percent * 100, 2)}%"
 
     def debug_log(self, index):
         index_str = f"index: {index},"
@@ -22,5 +33,6 @@ class DailyPriceInfo:
         max_value_str = f"最大值: {self.max_value},"
         min_value_str = f"最小值: {self.min_value},"
         avg_value_str = f"平均值: {self.average_price},"
-        loss_percent_str = f"回撤百分比: {round(self.retracement_percent * 100, 2)}%,"
+        # today_loss_percent_str = f"当天涨跌: {self.}"
+        loss_percent_str = f"回撤百分比: {round(self.loss_percent * 100, 2)}%,"
         print(index_str + date_str + max_value_str + min_value_str + avg_value_str + loss_percent_str)
