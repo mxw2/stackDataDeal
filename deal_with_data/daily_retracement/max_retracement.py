5ab1fdd2395860b6f9b7e4ecb72fd895ed3cff68
wangxin <1743998535@qq.com>
2024-04-20 13:06:15 +0800

    完成计算最大回撤功能，未验证

--- deal_with_data/daily_retracement/max_retracement.py
+++ deal_with_data/daily_retracement/max_retracement.py
@@ -22,13 +22,14 @@ column_index_for_min_value = 'E'
 # ********************************** 读取表内容 **********************************
 def read_data():
     # 读取数据源表，并且做若干判断，保证数据位置都是正确的
-    print("Maximum row: {0}".format(ds_sheet.max_row))
+
+    print("Maximum row: {0}, 有效行数量:{1}".format(ds_sheet.max_row, ds_sheet.max_row - 1))
     print("Maximum column: {0}".format(ds_sheet.max_column))
 
     price_infos = []
-    # 遍历每一行数据, range(5):  # 从0到4
-    # row是从1开始的，所以遍历的时候，必须要遍历到max_row + 1
-    # 例如：共11行数据，要遍历第2行-第11行数据
+    # 遍历每一行数据
+    # range(5):  # 从0到4
+    # range(1, 6):  # 从1开始，到5结束（5会被包含在内）
     for i in range(2, ds_sheet.max_row + 1):
         min_value = ds_sheet[column_index_for_min_value + str(i)].value
         max_value = ds_sheet[column_index_for_max_value + str(i)].value
@@ -38,6 +39,29 @@ def read_data():
                                                                 max_value,
                                                                 min_value,
                                                                 price_info.average_price))
+
+    # 按照时间由远到近的排序
+    price_infos.reverse()
+
+    # 统计最大亏损
+    max_loss = 0
+    max_loss_percent = 0
+    # 遍历每个价格 & 求出连续5天最大回撤
+    for i in range(len(price_infos)):
+        current_price = price_infos[i].average_price
+        for j in range(i + 1, i + target_days):
+            # 特殊处理，保证j < price_infos.count
+            if j >= len(price_infos):
+                print("遍历价格数组越界打印：i :{0}, j :{1}，continue".format(i, j))
+                continue
+            other_price = price_infos[j].average_price
+            if other_price < current_price:
+                temp_loss = other_price - current_price
+                max_loss = min(temp_loss, max_loss)
+                max_loss_percent = min(temp_loss / current_price, max_loss_percent)
+
+    # 打印最后的结果哈
+    print("最大损失: {0}, 最大回撤: {1}".format(max_loss, max_loss_percent))
 
 # ********************************** 启动 **********************************
 if __name__ == '__main__':
